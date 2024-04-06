330df10d029018eda6d71c72ae96544f8e19ad29
Darshika-25 <darshikaparekh25@gmail.com>
2024-04-07 03:02:18 +0500

    fix shebang

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-console.log(chalk.bold.bgGreen("\n\tWelcome to CLI Currency Converter App\n"))
+console.log(chalk.bold.bgGreen("\n\tWelcome to CLI Currency Converter App\n"));
 
 // 1) Choose a base currency and convert its values to other currencies e.g. USD
 
@@ -22,29 +24,52 @@ const currency: any = {
 
 let userAnswer = await inquirer.prompt([
   {
-    name: 'from',
+    name: "from",
     message: "Enter From Currency",
-    type: 'list',
-    choices: ['USD', 'EUR', 'GBP', 'JPY', 'INR', 'RUB', 'AED', 'KRW', 'PKR', 'ITL'],
-  },{
-    name: 'to',
-    message: "Enter To Currency",
-    type: 'list',
-    choices: ['USD', 'EUR', 'GBP', 'JPY', 'INR', 'RUB', 'AED', 'KRW', 'PKR', 'ITL'],
-  },{
-    name: 'amount',
-    message: "Enter Your Amount",
-    type: 'number',
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "JPY",
+      "INR",
+      "RUB",
+      "AED",
+      "KRW",
+      "PKR",
+      "ITL",
+    ],
   },
-  
+  {
+    name: "to",
+    message: "Enter To Currency",
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "JPY",
+      "INR",
+      "RUB",
+      "AED",
+      "KRW",
+      "PKR",
+      "ITL",
+    ],
+  },
+  {
+    name: "amount",
+    message: "Enter Your Amount",
+    type: "number",
+  },
 ]);
 
 // 3) Create dynamic data for mass calculations
 
-let fromAmount: any = currency [userAnswer.from]
-let toAmount: any = currency [userAnswer.to]
-let amount: any = userAnswer.amount
-let baseAmount: any = amount / fromAmount
-let convertedAmount: any = baseAmount * toAmount
+let fromAmount: any = currency[userAnswer.from];
+let toAmount: any = currency[userAnswer.to];
+let amount: any = userAnswer.amount;
+let baseAmount: any = amount / fromAmount;
+let convertedAmount: any = baseAmount * toAmount;
 
-console.log(chalk.bold.bgCyanBright(Math.floor(convertedAmount)))
+console.log(chalk.bold.bgCyanBright(Math.floor(convertedAmount)));
